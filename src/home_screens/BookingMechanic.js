29ca47d00596bebd07780739f453f1e3af37a690
adeelarif197 <adeelarif@hwryk.com>
2022-02-25 20:02:25 +0500

    Hire Now Done

--- src/home_screens/BookingMechanic.js
+++ src/home_screens/BookingMechanic.js
@@ -21,119 +21,118 @@ function BookingMechanic() {
 	]);
 
 	return (
-		<ScrollView contentContainerStyle={{ flex: 1 }}>
-			<View style={{ flex: 1, backgroundColor: 'white' }}>
-				<View style={{ flexDirection: 'row', padding: 20 }}>
-					<IconHeader
-						onleftPress={() => {
-							this.props.navigation.navigate('MechanicInfo');
-						}}
-						leftBtn={<AntDesign size={25} name="arrowleft" color={primaryColor} />}
-					/>
-					<Text style={{ ...headings.h5M, color: primaryColor, top: 5, left: 10 }}>{languages.booking}</Text>
+		<View style={{ flex: 1, backgroundColor: 'white' }}>
+			<View style={{ flexDirection: 'row', padding: 20 }}>
+				<IconHeader
+					onleftPress={() => {
+						this.props.navigation.navigate('MechanicInfo');
+					}}
+					leftBtn={<AntDesign size={25} name="arrowleft" color={primaryColor} />}
+				/>
+				<Text style={{ ...headings.h5M, color: primaryColor, top: 5, left: 10 }}>{languages.booking}</Text>
+			</View>
+
+			<View style={{}}>
+				<SwitchSelector
+					initial={0}
+					onPress={(value) => this.setState({ gender: value })}
+					selectedColor={'white'}
+					buttonColor={'#68397E'}
+					backgroundColor={'#F9ECFF'}
+					hasPadding={true}
+					height={50}
+					borderRadius={7}
+					style={{ marginHorizontal: 30 }}
+					options={[ { label: 'Hire now', value: 'h' }, { label: 'Schedule', value: 's' } ]}
+				/>
+			</View>
+			<View style={{ flex: 1, justifyContent: 'space-evenly' }}>
+				<View style={{}}>
+					<View style={{ marginLeft: '5%' }}>
+						<Text style={{ ...headings.h6, color: 'black' }}>{languages.selectime}</Text>
+					</View>
+
+					<View style={{ zIndex: 1000 }}>
+						<DropDownPicker
+							open={open}
+							value={value}
+							items={items}
+							setOpen={setOpen}
+							setValue={setValue}
+							setItems={setItems}
+							placeholder="9AM to 11AM"
+							placeholderStyle={{
+								color: 'grey',
+								fontWeight: 'bold',
+								textAlign: 'center'
+							}}
+							containerStyle={{
+								marginTop: '5%',
+								paddingHorizontal: 20
+							}}
+							dropDownContainerStyle={{
+								marginHorizontal: 20,
+								borderWidth: 0,
+								elevation: 5
+							}}
+							style={{
+								borderRadius: 0,
+								borderWidth: 0,
+								elevation: 5
+							}}
+							textStyle={{
+								fontSize: 15,
+								color: 'grey',
+								fontWeight: 'bold',
+								textAlign: 'center'
+							}}
+							maxHeight={200}
+						/>
+					</View>
 				</View>
 
 				<View style={{}}>
-					<SwitchSelector
-						initial={0}
-						onPress={(value) => this.setState({ gender: value })}
-						selectedColor={'white'}
-						buttonColor={'#68397E'}
-						backgroundColor={'#F9ECFF'}
-						hasPadding={true}
-						height={50}
-						borderRadius={7}
-						style={{ marginHorizontal: 30 }}
-						options={[ { label: 'Hire now', value: 'h' }, { label: 'Schedule', value: 's' } ]}
-					/>
-				</View>
-				<View style={{ flex: 1, padding: 10, justifyContent: 'space-between', bottom: 20 }}>
-					<View style={{ flex: 1 }}>
-						<View style={{ marginLeft: '5%', marginTop: '10%' }}>
-							<Text style={{ ...headings.h6, color: 'black' }}>{languages.selectime}</Text>
-						</View>
-
-						<View style={{ zIndex: 1000 }}>
-							<DropDownPicker
-								open={open}
-								value={value}
-								items={items}
-								setOpen={setOpen}
-								setValue={setValue}
-								setItems={setItems}
-								placeholder="9AM to 11AM"
-								placeholderStyle={{
-									color: 'grey',
-									fontWeight: 'bold',
-									textAlign: 'center'
-								}}
-								containerStyle={{
-									marginTop: '5%',
-									paddingHorizontal: 20
-								}}
-								dropDownContainerStyle={{
-									marginHorizontal: 20,
-									borderWidth: 0,
-									elevation: 5
-								}}
-								style={{
-									borderRadius: 0,
-									borderWidth: 0,
-									elevation: 5
-								}}
-								textStyle={{
-									fontSize: 15,
-									color: 'grey',
-									fontWeight: 'bold',
-									textAlign: 'center'
-								}}
-								maxHeight={200}
-							/>
-						</View>
-					</View>
-
 					<View style={{ marginLeft: '5%' }}>
 						<Text style={{ ...headings.h6, color: 'black' }}>{languages.description}</Text>
 					</View>
-
-					<TextInput
-          
+					<View
 						style={{
 							marginHorizontal: 20,
 							elevation: 2,
-							height: '35%',
-							width: '90%'
+							// flex:1,
+							height: '60%',
+
+							alignItems: 'flex-start'
+							// justifyContent:'flex-start',
+							// backgrou	ndColor:'red'
 						}}
-            
-					/>
-					{/* <View style={{}}>
+					>
 						<TextInput
 							style={{
-								marginHorizontal: 20,
-								elevation: 2,
-								height: '55%',
-								width: '90%',
-								
+								// flex:1
+								// height: '60%',
+								// width: '95%',
+								// paddingBottom: 140,
+								// marginLeft: '5%'
 							}}
 							placeholder="Describe your work requisites"
+							// placeholderStyle={{}}
 							autoFocus={false}
 							multiline={true}
 							textBreakStrategy={'balanced'}
 						/>
-					</View> */}
-				</View>
-
-				{/* <KeyboardAvoidingView>
-					<View style={{ }}>
-						<Btn1
-							lableStyle={{ ...headings.h6M, color: white }}
-							lable={languages.proceed}
-							onPress={() => this.props.navigation.navigate('Services')}
-						/>
 					</View>
-				</KeyboardAvoidingView> */}
+				</View>
 			</View>
-		</ScrollView>
+
+			<View style={{}}>
+				<Btn1
+					lableStyle={{ ...headings.h6M, color: white }}
+					lable={languages.proceed}
+					onPress={() => this.props.navigation.navigate('Services')}
+				/>
+			</View>
+		</View>
 	);
 }
 export default BookingMechanic;
